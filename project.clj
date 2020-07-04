9c8e6065638ecd5f93e6fcd339f74db19c145c95
Nandaja <nandaja.varma@gmail.com>
2020-07-04 19:05:20 +0200

    Adding Ring dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/nandajavarma/clojure-agendas.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.0"]]
   :repl-options {:init-ns clojure-agendas.core})

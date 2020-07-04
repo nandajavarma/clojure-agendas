42fad17fc5be4659214b78b43464df62d0208206
Nandaja <nandaja.varma@gmail.com>
2020-07-04 06:41:38 +0200

    Initial commit: Here it goes

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-agendas
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-agendas "0.1.0-SNAPSHOT"
+  :description "This is me trying to learn a bit of clojurescript"
+  :url "https://github.com/nandajavarma/clojure-agendas.git"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns clojure-agendas.core})

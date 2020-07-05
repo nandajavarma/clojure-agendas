07179eca80357f5d2df09b6f6474c1d4f53fa8fc
Nandaja <nandaja.varma@gmail.com>
2020-07-05 15:23:02 +0200

    Adding routes

--- src/clojure_agendas/core.clj
+++ src/clojure_agendas/core.clj
@@ -1,6 +1,50 @@
-(ns clojure-agendas.core)
+(ns clojure-agendas.core
+  (:require [ring.adapter.jetty :as webserver]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn welcome
+  [request]
+  {:status 200
+   :headers {}
+   :body "Hello, World"}
+   )
+
+(defn hello
+  [request]
+  (let [name (get-in request [:route-params :name])]
+  {:body (str "Hello, " name)
+   :status 200
+   :headers {}}))
+
+(defn about
+  [request]
+  {:status 200
+   :headers {}
+   :body "This is Nandaja trying to learn clojure"})
+
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/hello/:name" [name] hello)
+  (GET "/about" [] about)
+  (GET "/request-info" [] handle-dump)
+  (not-found "Endpoint not found"))
+
+(defn -dev-main
+  "This is the dev endpoint"
+  [port-number]
+  (webserver/run-jetty
+   (wrap-reload #'app)
+   {:port (Integer. port-number)
+    :join? false}
+   ))
+
+(defn -main
+  [port-number]
+  (webserver/run-jetty
+   app
+   {:port (Integer. port-number)}
+  ))
